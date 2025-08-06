5bc9282bbc74c7962aa30774bff40b517dc994b0
Shrawn <shrawn2002@gmail.com>
2025-08-06 22:08:12 +1000

    Implement Crud operations on the To-do list.

--- src/User.java
+++ src/User.java
@@ -1,10 +1,16 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class User {
     private String username;
     private String password;
+    private final ArrayList<ToDoItem> toDoItems;
+
 
     public User(String username, String password) {
         this.username = username;
         this.password = password;
+        this.toDoItems = new ArrayList<>();
     }
 
     /**
@@ -36,4 +42,19 @@ public class User {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    /**
+     * Gets the to-do items of the user.
+     * @return A reference to the list of to-do items, which can be modified from outside the class.
+     */
+    public ArrayList<ToDoItem> getToDoItems() {
+        return toDoItems;
+    }
+    public void add(ToDoItem item) {
+        this.toDoItems.add(item);
+    }
+
+
+
+
 }
